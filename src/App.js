560253ff489fec2c0ce91714dd99ffbf110c0528
Pablo <pablo@intvs.org>
2019-02-21 21:55:54 +0000

    Create a About component and added it to App

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import About from './Components/About';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          <About/>
           <a
             className="App-link"
             href="https://reactjs.org"
